835d9a187af8648accde5ce4c363a286d9b11c92
BEAN <bean@mealant.com>
2023-07-26 19:18:46 +0900

    5.2 Pause Play

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
+
   late Timer timer;
 
   void onTick(Timer timer) {
@@ -21,6 +23,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onStartPressed() {
     timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = !isRunning;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = !isRunning;
+    });
   }
 
   @override
@@ -48,8 +60,10 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(
+                  isRunning ? Icons.play_circle_outline : Icons.play_circle,
+                ),
               ),
             ),
           ),
