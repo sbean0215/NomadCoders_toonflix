16aad8c0c314645327cccf5e40e2ee49ee21a244
BEAN <bean@mealant.com>
2023-07-26 17:46:47 +0900

    4.3 BuildContext

--- lib/timer.dart
+++ lib/timer.dart
@@ -22,16 +22,20 @@ class _TimerState extends State<Timer> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
+              const MyLargeTitle(),
               for (var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 40,
@@ -41,6 +45,23 @@ class _TimerState extends State<Timer> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
